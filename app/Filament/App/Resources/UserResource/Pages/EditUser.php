9e623741cf6bf51b22dec90df3f3118e204c346d
XirTech <john.darlucio022@gmail.com>
2025-01-29 17:04:09 +0800

    User Resource Complete

--- app/Filament/App/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/App/Resources/UserResource/Pages/EditUser.php
@@ -10,10 +10,47 @@ class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
 
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('verifyEmail')
+                ->label('Verify Email')
+                ->icon('heroicon-m-envelope')
+                ->color('success')
+                ->visible(fn(\App\Models\User $record) => $record->email_verified_at === null)
+                ->action(function (\App\Models\User $record) {
+                    $record->email_verified_at = now();
+                    $record->save();
+
+                    \Filament\Notifications\Notification::make()
+                        ->success()
+                        ->title('Email Verified')
+                        ->body('User email has been verified successfully.')
+                        ->send();
+                }),
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function getSavedNotification(): ?\Filament\Notifications\Notification
+    {
+        $user = $this->record;
+
+        $auth = auth()->user();
+
+        $notification = \Filament\Notifications\Notification::make()
+            ->info()
+            ->icon('heroicon-o-finger-print')
+            ->title('Account Resource Modified')
+            ->body("Account {$user->name} Modified by {$auth->name}!")
+            ->actions([\Filament\Notifications\Actions\Action::make('View')->url(UserResource::getUrl('edit', ['record' => $user]))])
+            ->sendToDatabase([$user,$auth]);
+
+        return $notification;
     }
 }
